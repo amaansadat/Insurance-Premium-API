2c085db8ccb0d2dc26a9bf519aa77369f827bb65
Amaan <amaansadat707@gmail.com>
2026-06-24 15:39:25 +0530

    Connected frontend to Deployed API

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://insurance-premium-api-befz.onrender.com/predict"
 
 st.title("💊 Insurance Premium Predictor")
 st.markdown("Enter your details below:")
